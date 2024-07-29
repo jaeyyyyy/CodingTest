2d4b0727ee88481f36fcd2c7c773a29cbaf1d276
jaeyyyyy <145132400+jaeyyyyy@users.noreply.github.com>
2024-07-29 17:20:54 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -6,11 +6,10 @@ class Solution {
         
         int add = brown + yellow;
 
-        // 전체 타일의 약수를 찾습니다.
-        for (int height = 3; height <= Math.sqrt(add); height++) {
-            if (add % height == 0) {
-                int width = add / height;
-                if ((width - 2) * (height - 2) == yellow) {
+        for(int height = 3; height <= Math.sqrt(add); height++) { // 노란 격자가 있으려면 가로, 세로 한 변의 크기는 최소 3이상
+            if(add % height == 0) {
+                int width = add/height;
+                if((width-2) * (height - 2) == yellow) {
                     answer[0] = width;
                     answer[1] = height;
                 }
