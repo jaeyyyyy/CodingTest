b162273e9b9e418fd37219240701053c49a8bcc0
jaeyyyyy <145132400+jaeyyyyy@users.noreply.github.com>
2024-07-29 17:25:51 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,15 +1,13 @@
-import java.util.ArrayList;
-
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
         int add = brown + yellow;
 
-        for(int height = 3; height <= Math.sqrt(add); height++) { // 노란 격자가 있으려면 가로, 세로 한 변의 크기는 최소 3이상
+        for(int height = 3; height <= Math.sqrt(add); height++) {
             if(add % height == 0) {
                 int width = add/height;
-                if((width-2) * (height - 2) == yellow) {
+                if((width - 2) * (height - 2) == yellow) {
                     answer[0] = width;
                     answer[1] = height;
                 }
