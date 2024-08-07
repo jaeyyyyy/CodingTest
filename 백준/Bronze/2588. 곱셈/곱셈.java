ed00f94064619c8420b2b81c3a81c5e5a5a34377
jaeyyyyy <145132400+jaeyyyyy@users.noreply.github.com>
2024-08-07 18:22:12 +0900

    [Bronze III] Title: 곱셈, Time: 96 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,16 +9,14 @@ public class Main {
         int two = Integer.parseInt(bf.readLine());
         bf.close();
         
-        ArrayList<Integer> digits = new ArrayList<>();
-        int tem = two;
-        while(tem > 0) {
-            digits.add(tem % 10);
-            tem /= 10;
-        }
+        int c = two % 10;
+        int b = (two / 10) % 10;
+        int a = (two / 100) % 10;
         
-        for(int i : digits) {
-            System.out.println(one * i);
-        }
+        
+        System.out.println(one * c);
+        System.out.println(one * b);
+        System.out.println(one * a);
         System.out.println(one * two);
     }
 }
